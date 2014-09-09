96acee0fc3ffa388b311f3b8852724e777d386e2
Justin Ireland <justin@justinireland.com>
2014-09-09 15:45:46 -0700

    Reorganized things

--- public/js/app.js
+++ public/js/app.js
@@ -6,22 +6,56 @@ angular.module('myApp', [
   'myApp.controllers',
   'myApp.filters',
   'myApp.services',
-  'myApp.directives',
-  'ngRoute'
-]).
-config(function ($routeProvider, $locationProvider) {
-  $routeProvider.
-    when('/view1', {
-      templateUrl: 'partials/partial1',
-      controller: 'MyCtrl1'
-    }).
-    when('/view2', {
-      templateUrl: 'partials/partial2',
-      controller: 'MyCtrl2'
-    }).
-    otherwise({
-      redirectTo: '/view1'
-    });
+  'myApp.directives'
+]);
 
-  $locationProvider.html5Mode(true);
-});
+/*** Controllers ***/
+
+angular.module('myApp.controllers', []).
+	controller('AppCtrl', function ($scope, $http) {
+
+		$http({
+			method: 'GET',
+			url: '/api/name'
+		}).
+			success(function (data, status, headers, config) {
+				$scope.name = data.name;
+			}).
+			error(function (data, status, headers, config) {
+				$scope.name = 'Error!';
+			});
+
+	}).
+	controller('MyCtrl1', function ($scope) {
+		// write Ctrl here
+
+	}).
+	controller('MyCtrl2', function ($scope) {
+		// write Ctrl here
+
+	});
+
+/*** Directives ***/
+
+angular.module('myApp.directives', []).
+	directive('appVersion', function (version) {
+		return function(scope, elm, attrs) {
+			elm.text(version);
+		};
+	});
+
+/*** Filters ***/
+
+angular.module('myApp.filters', []).
+	filter('interpolate', function (version) {
+		return function (text) {
+			return String(text).replace(/\%VERSION\%/mg, version);
+		};
+	});
+
+/*** Services ***/
+
+// Demonstrate how to register services
+// In this case it is a simple value service.
+angular.module('myApp.services', []).
+	value('version', '0.1');
