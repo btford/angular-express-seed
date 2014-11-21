46fe2f07766b8cc8553812791d830e41a6fe66c4
rmyers <rmyers@euro-pro.com>
2014-11-20 19:33:00 -0500

    working in the examples from online.

--- app.js
+++ app.js
@@ -8,13 +8,86 @@ var express = require('express'),
   methodOverride = require('method-override'),
   //errorHandler = require('error-handler'),
   morgan = require('morgan'),
+  passport = require('passport'),
   routes = require('./routes'),
   api = require('./routes/api'),
   http = require('http'),
-  path = require('path')
+  path = require('path'),
+  LocalStrategy = require('passport-local').Strategy,
+  flash = require('connect-flash')
 
+
+
+var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+    , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+function findById(id, fn) {
+    var idx = id - 1;
+    if (users[idx]) {
+        fn(null, users[idx]);
+    } else {
+        fn(new Error('User ' + id + ' does not exist'));
+    }
+}
+
+function findByUsername(username, fn) {
+    for (var i = 0, len = users.length; i < len; i++) {
+        var user = users[i];
+        if (user.username === username) {
+            return fn(null, user);
+        }
+    }
+    return fn(null, null);
+}
+
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+
+// Use the LocalStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.  In the real world, this would query a database;
+//   however, in this example we are using a baked-in set of users.
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+
+            // Find the user by username.  If there is no user with the given
+            // username, or the password is not correct, set the user to `false` to
+            // indicate failure and set a flash message.  Otherwise, return the
+            // authenticated `user`.
+            findByUsername(username, function(err, user) {
+                if (err) { return done(err); }
+                if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+                if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+                return done(null, user);
+            })
+        });
+    }
+));
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/login')
+}
 var app = module.exports = express()
-
 
 /**
  * Configuration
@@ -24,10 +97,13 @@ var app = module.exports = express()
 app.set('port', process.env.PORT || 8000)
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
-app.use(morgan('dev'))
+//app.use(morgan('dev'))
 app.use(bodyParser())
 app.use(methodOverride())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 
 var env = process.env.NODE_ENV || 'development';
 
@@ -47,15 +123,21 @@ if (env === 'production') {
  */
 
 // serve index and view partials
-app.get('/', routes.index)
+app.get('/', ensureAuthenticated, routes.index)
 app.get('/partials/:name', routes.partials)
+app.get('/login', function(req, res){
+    console.log('should login')
+})
 
 // JSON API
 app.get('/api/name', api.name)
 app.get('/api/watable_data',api.watable_data)
 
 // redirect all others to the index (HTML5 history)
-app.get('*', routes.index)
+app.get('*', function(req, res){
+    console.log('default called, should login')
+    res.redirect('/login')
+})
 
 
 /**
