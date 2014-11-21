291ede36016f17c0245220b06fc95bb98caaabb8
rmyers <rmyers@euro-pro.com>
2014-11-20 19:33:00 -0500

    working in the examples from online.

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,12 @@
  */
 
 exports.index = function(req, res){
-  res.render('index')
+    console.log('calling index')
+  res.render('index', { user: req.user })
 }
 
 exports.partials = function (req, res) {
   var name = req.params.name
   console.log("name == " + name)
-  res.render('partials/' + name)
+  res.render('partials/' + name, { user: req.user })
 }
