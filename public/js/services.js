19f46b9c15f4d38cf6c97ad37493229d2954cb3d
xqjibz <ccrider@whiterose.net>
2014-12-08 23:45:17 -0500

    nope

--- public/js/services.js
+++ public/js/services.js
@@ -8,4 +8,14 @@
 (function() {
     angular.module('myApp.services', []).
         value('version', '0.1')
+    angular.module.factory('userInfoSvc', ['', function(){
+        var userInfoSvc = {
+            getUserName : function(){
+                console.log('returning username infoz')
+
+            }
+
+        }
+        return userInfoSvc
+    }])
 })()
