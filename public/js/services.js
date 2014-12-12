375cd220c9d381babbb2c5489a4fd00f4d58895d
xqjibz <ccrider@whiterose.net>
2014-12-12 16:28:05 -0500

    well that's amazing.

--- public/js/services.js
+++ public/js/services.js
@@ -6,15 +6,15 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 (function() {
-    angular.module('myApp.services', []).
-        value('version', '0.1')
+    angular.module('myApp.services', [])
+        .value('version', '0.1')
     // return user info and permissions
-    angular.module.factory('userInfoSvc', ['$http', function($http){
-        var userInfoSvc = {
-            getUserName : function(){
-                console.log('returning username infoz')
+        .factory('userInfoSvc', ['$http', function($http){
+            var userInfoSvc = {
+                getUserName : function(){
+                    console.log('returning username infoz')
 
-            }
+                }
 
         }
         return userInfoSvc
