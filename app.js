e17f778d24692fff45f737d10acf3bf6847f631f
ecapswen <dante.peng@gmail.com>
2016-03-11 01:06:40 -0800

    change back to port 3000

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var app = module.exports = express();
  */
 
 // all environments
-app.set('port', process.env.PORT || 8081);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(morgan('dev'));
